8f85a8f76a42f0baac3ea6539736941c89c936c1
Raydowi <54383151+Raydowi@users.noreply.github.com>
2023-10-25 10:39:18 -0600

    Created 2D Illustrations section and began adding pages for Software Dev projects (including a simple page for the Ray Tracing project)

--- Portfolio/PortfolioWebApp/Controllers/PortfolioController.cs
+++ Portfolio/PortfolioWebApp/Controllers/PortfolioController.cs
@@ -9,12 +9,33 @@ namespace PortfolioWebApp.Controllers
             return View();
         }
 
+        //Game Dev Portfolio Pages
+
         public IActionResult WhiskerWitchery()
+        {
+            return View();
+        }
+        public IActionResult SeniorHearts()
         {
             return View();
         }
 
         public IActionResult LeafItBehind()
+        {
+            return View();
+        }
+        public IActionResult HiveQuest()
+        {
+            return View();
+        }
+
+        //Software Dev Pages
+        public IActionResult HTTPProxy()
+        {
+            return View();
+        }
+
+        public IActionResult RayTracing()
         {
             return View();
         }
